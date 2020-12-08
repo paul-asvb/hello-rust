c28c7f7a07db38562da9c7b0e09089e8a8380d42
Paul <paul@paul-mayer.eu>
2020-12-08 22:44:58 +0100

    async request example

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use rand::Rng;
 use std::io;
 
-fn main() {
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+use std::collections::HashMap;
 
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
+
+fn test() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
     let mut guess = String::new();
     println!("Guess the number!");
