70a0a4f6adac245823c38dd8d30b7838296ab0ac
paul work <paulwork@pauls-MBP.fritz.box>
2020-12-13 20:37:30 +0100

    test image download

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .json::<HashMap<String, String>>()
         .await?;
     println!("{:#?}", resp);
+
+    let mut client = reqwest::Client::new();
+    let mut image_file = client
+        .get("https://images.pexels.com/photos/2124773/pexels-photo-2124773.jpeg")
+        .send()
+        .unwrap();
+
+    let path = Path::new("img_test.jpeg");
+    let display = path.display();
+    let mut file = match File::create(&path) {
+        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+        Ok(file) => file,
+    };
+    match std::io::copy(&mut image_file, &mut file) {
+        Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
+        Ok(_) => println!("successfully wrote to {}", display),
+    }
+
     Ok(())
 }
 
