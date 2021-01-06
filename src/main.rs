f42018c9aefb6da3a7ffba3780d3805f342dc355
Paul Mayer <paul@paul-mayer.eu>
2021-01-06 10:25:52 +0100

    get image withreqwest

--- src/main.rs
+++ src/main.rs
@@ -13,24 +13,24 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
     println!("{:#?}", resp);
 
-   /* let mut client = reqwest::Client::new();
-    let mut image_file = client
+    let client = reqwest::Client::new();
+    let image_file = client
         .get("https://images.pexels.com/photos/2124773/pexels-photo-2124773.jpeg")
-        .send();
-
-*/
-/*
-    let path = Path::new("img_test.jpeg");
-    let display = path.display();
-    let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}", display, why.description()),
-        Ok(file) => file,
-    };
-    match std::io::copy(&mut image_file, &mut file) {
-        Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
-        Ok(_) => println!("successfully wrote to {}", display),
-    }
-*/
+        .send()
+        .await?;
+    println!("{:#?}", image_file);
+    /*
+        let path = Path::new("img_test.jpeg");
+        let display = path.display();
+        let mut file = match File::create(&path) {
+            Err(why) => panic!("couldn't create {}: {}", display, why.description()),
+            Ok(file) => file,
+        };
+        match std::io::copy(&mut image_file, &mut file) {
+            Err(why) => panic!("couldn't write to {}: {}", display, why.description()),
+            Ok(_) => println!("successfully wrote to {}", display),
+        }
+    */
     Ok(())
 }
 
